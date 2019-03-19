ef8f6d8e078a3dd881f60dcb1541f8dd653cb729
thomasdesgrippes <thomas@horsealot.com>
2019-03-19 14:12:56 +0100

    Create robots.txt

--- src/components/Products.js
+++ src/components/Products.js
@@ -18,11 +18,11 @@ class Group extends Component {
                             <img src={'/assets/images/trictrac.png'} title='Trictrac Show logo' alt='Trictrac Show logo'/>
                             <div className='products__product-title trictrac'>trictrac</div>
                         </a>
-                        <a href='' className='products__product flex flex--center-center flex--column'>
+                        <a href='https://www.horsealot.com' className='products__product flex flex--center-center flex--column'>
                             <img src={'/assets/images/horsealot.png'} title='Horsealot logo' alt='Horsealot logo'/>
                             <div className='products__product-title horsealot'>Horsealot</div>
                         </a>
-                        <a href='' className='products__product flex flex--center-center flex--column'>
+                        <a href='https://brain.horsealot.com' className='products__product flex flex--center-center flex--column'>
                             <img src={'/assets/images/brain.png'} title='Brain logo' alt='Brain logo'/>
                             <div className='products__product-title brain'>BRAIN</div>
                         </a>
